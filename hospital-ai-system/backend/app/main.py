f06a24c33805fe7ba2f4fc2166543c5b4f5ee94c
shereef888k <85346504+shereef888k@users.noreply.github.com>
2026-03-17 11:37:36 +0530

    fix: cors for vercel

--- hospital-ai-system/backend/app/main.py
+++ hospital-ai-system/backend/app/main.py
@@ -19,11 +19,14 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://hospital-ai-system-gaz9.vercel.app",
+        "https://hospital-ai-system-theta.vercel.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(triage_router)
 app.include_router(patients_router)
